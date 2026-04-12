ec7a6d94a8aeb5e22fa3f24b833bd0ec7ccf1c31
Lukas Winkler <git@lw1.at>
2026-04-12 22:56:25 +0200

    make "route" default colorMode

--- ui/src/routes/+page.svelte
+++ ui/src/routes/+page.svelte
@@ -85,7 +85,7 @@
 					: 'connections')
 	);
 	let dataAttributionLink: string | undefined = $state(undefined);
-	let colorMode = $state<'rt' | 'route' | 'mode' | 'none'>(isSmallScreen ? 'none' : 'rt');
+	let colorMode = $state<'rt' | 'route' | 'mode' | 'none'>(isSmallScreen ? 'none' : 'route');
 	let showMap = $state(!isSmallScreen);
 	let showRoutes = $state(false);
 	let routesOverlaySession = $state(0);
